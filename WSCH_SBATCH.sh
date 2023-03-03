abb832d16aceaefa8bbce99605bf21b95a8aea61
TominKapp <tkapp001@ucr.edu>
2023-03-03 06:23:03 -0800

    Added SBATCH file

---
+++ WSCH_SBATCH.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=10G
+#SBATCH --time=1-00:15:00
+#SBATCH --mail-user=useremail@address.com
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="workshop_test"
+#SBATCH -p intel
+
+module purge
+module load hpcc_workshop/2.0
+module load miniconda3
+
+mkdir -p output
+rm -rf output/output.txt
+create_output_file > output/output.txt
+
+chmod 000 output/output.txt
